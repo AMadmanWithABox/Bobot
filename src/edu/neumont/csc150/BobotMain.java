8b0f5053928abb67f0da3f88f242e625806b9caf
MatthewM5293 <mmunozramirez@student.neumont.edu>
2022-02-28 17:50:41 -0700

    Called BobotFrame in BobotMain

--- src/edu/neumont/csc150/BobotMain.java
+++ src/edu/neumont/csc150/BobotMain.java
@@ -1,4 +1,10 @@
 package edu.neumont.csc150;
 
+import edu.neumont.csc150.view.BobotFrame;
+
 public class BobotMain {
+    public static void main(String[] args) {
+        BobotFrame frame = new BobotFrame();
+        frame.frameStuff();
+    }
 }
