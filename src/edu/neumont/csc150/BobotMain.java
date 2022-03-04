1e1bcfeec2bd166bae1361bf55e08bd7f4f09754
honeybadger448 <36938347+honeybadger448@users.noreply.github.com>
2022-03-03 21:23:48 -0700

    test

--- src/edu/neumont/csc150/BobotMain.java
+++ src/edu/neumont/csc150/BobotMain.java
@@ -2,12 +2,18 @@ package edu.neumont.csc150;
 
 import edu.neumont.csc150.model.Camera;
 import edu.neumont.csc150.view.BobotFrame;
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.connection.channel.direct.Session;
+
+import java.io.IOException;
 
 public class BobotMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        BobotFrame frame = new BobotFrame();
 //        frame.frameStuff();
         Camera bobotCam = new Camera();
         bobotCam.run();
+
+
     }
 }
--- src/edu/neumont/csc150/model/Camera.java
+++ src/edu/neumont/csc150/model/Camera.java
@@ -1,59 +1,78 @@
 package edu.neumont.csc150.model;
 
 import com.github.sarxos.webcam.*;
+import com.github.sarxos.webcam.ds.cgt.WebcamCloseTask;
+import com.github.sarxos.webcam.ds.cgt.WebcamReadBufferTask;
+import com.github.sarxos.webcam.ds.ipcam.IpCamDriver;
+import com.github.sarxos.webcam.util.ImageUtils;
+import com.sun.source.util.TaskEvent;
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.connection.channel.direct.Session;
+import net.schmizz.sshj.connection.channel.direct.SessionChannel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Camera implements WebcamMotionListener {
 
-    Webcam myWebcam = Webcam.getWebcams().get(0);
-    WebcamMotionDetector md = new WebcamMotionDetector(myWebcam);
-    JFrame window = new JFrame("Robot View");
-    WebcamPanel panel = new WebcamPanel(myWebcam);
+    WebcamPanel panel;
+    WebcamMotionDetector md;
 
-    public void run() {
+    public void run() throws IOException {
+
+        Webcam myWebcam = Webcam.getDefault();
+        myWebcam.close();
+        myWebcam.setViewSize(WebcamResolution.VGA.getSize());
+        myWebcam.open();
+        JFrame window = new JFrame("Robot View");
+        md = new WebcamMotionDetector(Webcam.getDefault());
+        panel = new WebcamPanel(Webcam.getDefault());
 
         panel.setFPSDisplayed(true);
         panel.setDisplayDebugInfo(true);
         panel.setImageSizeDisplayed(true);
-        panel.setMirrored(false);
-        System.out.println(panel.getAlignmentX());
-        System.out.println(panel.getAlignmentY());
+        panel.setMirrored(true);
 
         window.add(panel);
-        window.setBounds(0,0,320,180);
         window.setResizable(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
+        window.pack();
         window.setLayout(null);
-        window.setSize(320,180);
 
         md.setInterval(100); // one check per 500 ms
         md.addMotionListener(this);
         md.start();
-        md.setPixelThreshold(20);
         md.clearInertia();
+        md.setPixelThreshold(30);
+
+        SSHClient sshClient = new SSHClient();
+        sshClient.connect("192.168.37.18", 22);
+        sshClient.authPassword("pi","BeepBoop");
+        System.out.println(sshClient.getConnection());
+        Session.Command exec = sshClient.startSession().exec("python3 motorTest.py");
+        sshClient.disconnect();
 
     }
 
     @Override
     public void motionDetected(WebcamMotionEvent webcamMotionEvent) {
 
-        System.out.println("movement is on the " + getYDirectionOfMovement(webcamMotionEvent,webcamMotionEvent.getCog().getY()) + " " + getXDirectionOfMovement(webcamMotionEvent, webcamMotionEvent.getCog().getX()));
         Graphics g = panel.getGraphics();
         g.setPaintMode();
         g.setColor(Color.green);
-        int x = webcamMotionEvent.getCog().x;
-        int y = webcamMotionEvent.getCog().y;
-        g.drawOval(x,y,100,100);
+        g.drawOval(md.getMotionCog().getLocation().x,md.getMotionCog().getLocation().y,100,100);
+
+        System.out.println("movement is detected on the " + getYDirectionOfMovement(webcamMotionEvent,webcamMotionEvent.getCog().getLocation().y) + " " + getXDirectionOfMovement(webcamMotionEvent,webcamMotionEvent.getCog().getLocation().x));
+
 
     }
 
     public String getXDirectionOfMovement(WebcamMotionEvent webcamMotionEvent, double x){
-        if(x > 160 ){
+        if(x > 320 ){
             return "right";
         }else {
             return "left";
@@ -61,7 +80,7 @@ public class Camera implements WebcamMotionListener {
     }
 
     public String getYDirectionOfMovement(WebcamMotionEvent webcamMotionEvent, double y){
-        if (y > 90){
+        if (y > 220){
             return "bottom";
         }else{
             return "top";
