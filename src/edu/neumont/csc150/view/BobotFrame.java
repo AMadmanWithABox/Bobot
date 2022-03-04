b075ebdfcc2298e257d9f34437cd7c71ef48a4c5
MatthewM5293 <mmunozramirez@student.neumont.edu>
2022-03-03 19:52:32 -0700

    fixed window size

--- src/edu/neumont/csc150/view/BobotFrame.java
+++ src/edu/neumont/csc150/view/BobotFrame.java
@@ -19,23 +19,30 @@ public class BobotFrame {
         JButton followButton = new JButton("Follow");
         JButton detectButton = new JButton("Object Detection");
         JButton controlButton = new JButton("Control");
-
         //font type
         String fontForText = "Monospaced";
         int titles = 20;
-
         //Frame settings
         mainFrame.getContentPane().setBackground(Color.DARK_GRAY);
         mainFrame.setBounds(100, 100, 350, 600);
         mainFrame.setResizable(false);
         mainFrame.setLayout(null);
-
         //TextArea
         mainMenuText.setFont(new Font(fontForText, Font.BOLD, titles));
         mainMenuText.setLineWrap(true);
         mainMenuText.setEditable(false);
         mainMenuText.setBackground(Color.DARK_GRAY);
-
+        mainMenuText.setForeground(Color.WHITE);
+        mainMenuText.setBounds(120, 100, 150, 100);
+        //Button1
+        followButton.setBackground(Color.WHITE);
+        followButton.setForeground(Color.BLACK);
+        followButton.setBounds(100, 200, 150, 60);
+        //Button2
+        detectButton.setBackground(Color.WHITE);
+        detectButton.setForeground(Color.BLACK);
+        detectButton.setBounds(100, 300, 150, 60);
+        //Button3
         controlButton.setBackground(Color.WHITE);
         controlButton.setForeground(Color.BLACK);
         controlButton.setBounds(100, 400, 150, 60);
